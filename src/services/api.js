e33a555c21bfe9103aa50f2f44b627e64ce2e641
Yasmin Menezes <104459939+yasminmenezes@users.noreply.github.com>
2024-05-15 23:58:13 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,9 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'http://192.168.0.10:3000', //Casa
-  // baseURL: 'http://192.168.3.49:3000', //Mamanguape,
-  // baseURL: 'http://10.10.2.42:3000', //Jaguaribe
-  // baseURL: 'http://192.168.0.4:3000', //Moises
+  // baseURL: 'http://192.168.3.49:3000', //Mamanguape
 });
 
 export default api;
